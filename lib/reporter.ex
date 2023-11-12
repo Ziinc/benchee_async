46d86d9abd687d3322014bd0bbe23dd2a462d5f5
TzeYiing <ty@tzeyiing.com>
2023-11-12 16:45:34 +0800

    docs: add moduledoc for BencheeAsync.Reporter

--- lib/reporter.ex
+++ lib/reporter.ex
@@ -1,5 +1,50 @@
 defmodule BencheeAsync.Reporter do
-  @moduledoc false
+  @moduledoc """
+  The GenServer responsible for tracking units of work done.
+
+  To allow for more flexibility, this GenServer must be started manually before beginning the benchmarking as part of the benchmark suite setup.
+
+  ```elixir
+  # in an .exs script
+  #{__MODULE__}.start_link()
+
+  # in an ExUnit test
+  start_supervised!(#{__MODULE__})
+  ```
+
+  ### Reporting
+  To report a unit of work done, execute `#{__MODULE__}.report/0` anywhere within your application code.
+
+  To avoid introducing dev-specific logic into your application code, it is advised to use a mocking library to mock your internal functions. [:meck](https://github.com/eproxus/meck) or [Mimic](https://github.com/edgurgel/mimic) will both work, the choice between either or otherwise would be a matter of taste.
+
+  For example, with Mimic:
+  ```
+  # test/test_helper.exs
+  Mimic.copy(MyApp)
+  ExUnit.start()
+  ```
+
+  ```
+  # test/my_app_test.exs
+  defmodule MyAppTest do
+    use ExUnit.Case, async: false
+    use Mimic
+
+    @tag :benchmark
+    test "benchmarking async work!" do
+      MyApp
+      |> stub(:do_work, fn _arg ->
+        #{__MODULE__}.report()
+        :ok
+      end)
+
+      # benchmarking code goes here...
+      BencheeAsync.run(%{....})
+    end
+  end
+  ```
+
+  """
   use GenServer
 
   def start_link(opts \\ []) do
@@ -37,26 +82,40 @@ defmodule BencheeAsync.Reporter do
   end
 
   @doc """
-  Records a unit of work done. Should be called each time a unit of work is performed. 
-  
-  It is advised to mock the function that you wish to track and call this function within the mock. 
+  Records a unit of work done. Should be called each time a unit of work is performed.
+
+  It is advised to mock the function that you wish to track and call this function within the mock.
   """
+  @spec record() :: :ok
   def record() do
     GenServer.cast(__MODULE__, :record)
   end
 
   @doc """
-  Retrieves the samples recorded for a given scenario and input combination. 
+  Retrieves the samples recorded for a given scenario and input combination.
   """
+  @spec get_samples(String.t(), term() | :__no_input) :: [integer()]
   def get_samples(scenario_name, input \\ :__no_input) when is_binary(scenario_name) do
     GenServer.call(__MODULE__, {:samples, scenario_name, :erlang.phash2(input)})
   end
 
-  @doc false
+  @doc """
+  Resets the state within the Reporter. This is not called automatically after the benchmark is run, as it is expected that the Reporter process terminates after each benchmark run.
+
+  If this is not the case for you, you can run this manually between benchmark runs as so:
+
+  ```elixir
+  BencheeAsync.run(%{...})
+  BencheeAsync.Reporter.clear()
+  BencheeAsync.run(%{...})
+  ```
+
+  If the scenario names are different between each benchmark run, then clearing the state would not be necessary.
+  """
+  @spec clear() :: :ok
   def clear() do
     GenServer.call(__MODULE__, :clear)
   end
-
 
   # GenServer
   @impl GenServer
